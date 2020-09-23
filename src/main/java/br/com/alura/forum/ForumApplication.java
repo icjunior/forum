c6de3b723d1f622db100cf2c11318687239597af
Ismael Junior <icjunior@bigsupermercados.com.br>
2020-09-23 17:59:40 -0300

    Primeiro;

---
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@EnableCaching
+@EnableSpringDataWebSupport
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
